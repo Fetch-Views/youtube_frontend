e33b9aed73ad682afdde74bc31c9dc9d0e0468e4
DeepLeau <thomas.bodenan@gmail.com>
2025-01-22 13:57:38 +0100

    test getcookie

--- scripts/auth.js
+++ scripts/auth.js
@@ -128,7 +128,9 @@ async function checkAuth() {
 
 function getCookie(name) {
     const value = `; ${document.cookie}`;
+    console.log(value)
     const parts = value.split(`; ${name}=`);
+    console.log(parts)
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
