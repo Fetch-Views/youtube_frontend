a49810073517b3f96e8692560aa2f6910f758b17
DeepLeau <thomas.bodenan@gmail.com>
2025-02-03 23:54:35 +0100

    profile url fixed

--- scripts/auth.js
+++ scripts/auth.js
@@ -139,7 +139,7 @@ async function refreshToken() {
 
 async function checkAuth() {
     try {
-        let response = await fetchWithAuth('https://web-production-5b55f.up.railway.app/api/users/profile/');
+        let response = await fetchWithAuth('https://web-production-5b55f.up.railway.app/api/users/user_profile/');
 
         if (response) {
             localStorage.setItem('email', response.email);
